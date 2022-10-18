d045cf549754d091980c3a151249908b7e3d551e
Jackson Jones <63122233+JacksonKJones@users.noreply.github.com>
2022-10-17 21:45:43 -0400

    Correction

--- Assignment01/src/chapter07/E7_33.java
+++ Assignment01/src/chapter07/E7_33.java
@@ -1,9 +1,22 @@
 package chapter07;
 
+import java.util.Scanner;
+
 public class E7_33 {
 
 	public static void main(String[] args) {
 		
+		Scanner input = new Scanner(System.in);
+
+		String[] zodiacs = {"monkey", "rooster", "dog", "pig", "rat", "ox", "tiger",
+									"rabbit", "dragon", "snake", "horse", "sheep"};
+
+		System.out.print("Enter a year: ");
+		int year = input.nextInt();
+
+		System.out.println(year + " is the year of the " + zodiacs[year % 12]);
+		
+		input.close();
 		
 	}
 	
